0edcc90e6d06b42469286f5d5c661a7fb26b1df5
a-kataoka <a-kataoka@cookbiz.jp>
2020-08-31 17:46:59 +0900

    build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,52 +7,38 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Font from '@ckeditor/ckeditor5-font/src/font';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
-// import Link from '@ckeditor/ckeditor5-link/src/link';
+import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-// import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-// import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	// UploadAdapter,
+	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
-	Underline,
-	Strikethrough,
-	Subscript,
-	Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -62,21 +48,15 @@ ClassicEditor.builtinPlugins = [
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	ImageResize,
-	// Link,
+	Indent,
+	Link,
 	List,
+	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	Base64UploadAdapter,
-	// MediaEmbed,
-	Highlight,
-	Indent,
-	IndentBlock,
-	// TextTransformation,
-	Font,
-	Alignment
+	TextTransformation
 ];
 
 // Editor configuration.
@@ -85,53 +65,29 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'fontSize',
-			'fontFamily',
-			'fontColor',
-			'fontBackgroundColor',
-			'|',
 			'bold',
 			'italic',
-			'underline',
-			'strikethrough',
-			'blockQuote',
-			'subscript',
-			'superscript',
-			'|',
-			'highlight',
-			'outdent',
-			'indent',
-			'outdent',
-			'alignment',
+			'link',
 			'bulletedList',
 			'numberedList',
 			'|',
-			// 'link',
+			'indent',
+			'outdent',
+			'|',
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
-			// 'mediaEmbed',
+			'mediaEmbed',
 			'undo',
 			'redo'
 		]
 	},
 	image: {
 		toolbar: [
-			'imageTextAlternative',
-			'|',
-			'imageStyle:alignLeft',
 			'imageStyle:full',
-			'imageStyle:alignRight'
-		],
-		styles: [
-			// This option is equal to a situation where no style is applied.
-			'full',
-
-			// This represents an image aligned to the left.
-			'alignLeft',
-
-			// This represents an image aligned to the right.
-			'alignRight'
+			'imageStyle:side',
+			'|',
+			'imageTextAlternative'
 		]
 	},
 	table: {
